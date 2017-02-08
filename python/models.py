06aeddb922f9124904e26073b414dafe846fc178
Fernando Ghinzelli <fghinzelli@mprs.mp.br>
2017-02-08 18:51:25 -0200

    Perfis

--- python/models.py
+++ python/models.py
@@ -6,10 +6,38 @@ class Perfil(object):
 		self.nome = nome
 		self.telefone = telefone
 		self.empresa = empresa
+		self.__curtidas = 0
 
 
 	def imprimir(self):
 		print "Nome: %s, Telefone: %s, Empresa: %s" % (self.nome, self.telefone, self.empresa)
+
+	def curtir(self):
+		self.__curtidas += 1
+
+	def obter_curtidas(self):
+		return self.__curtidas
+
+	@classmethod
+	def gerar_perfis(classe, nome_arquivo):
+		arquivo = open(nome_arquivo,'r')
+		perfis = []
+		for linha in arquivo:
+			valores = linha.split(',')
+			perfis.append(Perfil(*valores))
+		arquivo.close()
+		return perfis
+
+
+class Perfil_Vip(Perfil):
+	'Classe padrao para perfis vip'
+	def __init__(self, nome, telefone, empresa, apelido=''):
+		super(Perfil_Vip, self).__init__(nome, telefone, empresa)
+		self.apelido = apelido
+
+	def obter_creditos(self):
+		return super(Perfil_Vip, self).obter_curtidas() * 10.0
+
 
 class Data(object):
 	'Classe para exibir datas formatadas'
