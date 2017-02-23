0e527a5fdb1472a0cd57c422f00e40deecc036c0
Fernando Ghinzelli <fghinzelli@gmail.com>
2017-02-23 09:12:30 -0300

    Dicionarios

--- python/readFiles.py
+++ python/readFiles.py
@@ -12,17 +12,26 @@ def sanitize(time_string):
 	(min, secs) = time_string.split(splitter)
 	return (min + '.' + secs)
 
-def read_file(file_name):
+def get_coach_data(file_name):
 	try:
 		with open(file_name) as ln:
 			data = ln.readline()
-		return(data.strip().split(','))
+
+		data = data.strip().split(',')
+		sarah_data = {'Name': data.pop(0),
+					  'DOB': data.pop(0),
+					  'Times': str(sorted(set([sanitize(each_item) for each_item in data]))[0:3])}
+
+		return(sarah_data)
 	except IOError as err:
 		print("Ocorreu um erro: " + err)
 		return None
 
-print(sorted(set([sanitize(each_item) for each_item in read_file("james.txt")]))[0:3])
-print(sorted(set([sanitize(each_item) for each_item in read_file("julie.txt")]))[0:3])
-print(sorted(set([sanitize(each_item) for each_item in read_file("mikey.txt")]))[0:3])
-print(sorted(set([sanitize(each_item) for each_item in read_file("sarah.txt")]))[0:3])
-
+james_data = get_coach_data('james2.txt')
+print(james_data['Name'] + "'s fastest times are: " + james_data['Times'])  
+julie_data = get_coach_data('julie2.txt')
+print(julie_data['Name'] + "'s fastest times are: " + julie_data['Times'])  
+mikey_data = get_coach_data('mikey2.txt')
+print(mikey_data['Name'] + "'s fastest times are: " + mikey_data['Times'])  
+sarah_data = get_coach_data('sarah2.txt')
+print(sarah_data['Name'] + "'s fastest times are: " + sarah_data['Times'])
